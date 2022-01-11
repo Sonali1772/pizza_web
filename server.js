318acc6903cd5f77e63bab8a30ca49498ea3a5e3
sonali pandey <56030380+Sonali1772@users.noreply.github.com>
2022-01-11 16:30:08 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,30 @@
+// express server setup
+
+//importing all dependency
+
+const express = require('express')
+const app = express()
+const ejs = require('ejs')
+const path = require('path')
+const expressLayout = require('express-ejs-layouts')
+const PORT = process.env.PORT || 3000
+
+//assets
+app.use(express.static('public'))
+
+//home.ejs file (like html main file setup)
+
+app.get('/', (req, res) => {
+    res.render('home')              
+})
+
+//set template engine
+
+app.use(expressLayout)
+app.set('views', path.join(__dirname, '/resources/views'))
+app.set('view engine' , 'ejs')
+
+
+app.listen(PORT , () => {
+    console.log('listening on port 3000')
+})
